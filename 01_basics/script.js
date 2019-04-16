af120e912c158ea16b2e8960164bb6c04490cc83
ThisIsSandeepB <sb.isandeepbansal@gmail.com>
2019-04-16 11:56:13 +0530

    Basics of variables and data type

--- 01_basics/script.js
+++ 01_basics/script.js
@@ -1,1 +1,35 @@
-console.log('Hello World!!');
+/* -------------------------------------------------- */
+/* VARIABLES */
+/* -------------------------------------------------- */
+
+var firstName = 'Micheal';
+var lastName = 'Barak';
+var age = 42;
+
+/* 
+  Above we have declare 3 variables firstName, lastName and age. two of them are String type and one is Number type let's see what kind of data we can store inside variables:
+
+  PRIMITIVE DATA TYPES (VALUE TYPE):
+  1. Number
+  2. String
+  3. Boolean
+  4. undefined (By default the type of variable is undefined)
+  5. null
+  6. Symbol
+
+  REFERENCE DATA TYPES (OBJECT TYPE):
+
+  NOTE: JavaScript has dynamic typing: data types are automatically assigned to variables.
+*/
+
+var fullAge = true;
+var job;
+console.log(fullAge);
+console.log(job);
+
+/* 
+  There are some rules regarding naming convention of variables and it is recommended to use camelCase for variable name.
+  1. cannot start with symbol and number except (_ and $)
+  2. cannot have symbol in the middle
+  3. we cannot use javascript reserved keyword as variable name (like: for, if, while etc).
+*/
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Javascript
 
-**Javascript is a lightweight, cross-platform, object-oriented computer programming language** :smile:
+##**Javascript is a lightweight, cross-platform, object-oriented computer programming language** :smile:
 
-- Javascript is one of the three core technologies of web development.
+- Javascript is one of the three core technologies of web development (HTML, CSS and JS).
 - Today, Javascript can be used in different places:
 
   - Client-side: Javascript was traditionally only used in the browser.
